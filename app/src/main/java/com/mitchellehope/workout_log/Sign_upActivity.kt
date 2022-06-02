437c8c56995095a2c035a9d16c6fec2b31c25c24
mitchelle-hope <mitchellehopewaithera@gmail.com>
2022-06-02 22:50:04 +0300

    added signup page

---
+++ app/src/main/java/com/mitchellehope/workout_log/Sign_upActivity.kt
@@ -0,0 +1,11 @@
+package com.mitchellehope.workout_log
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_upActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
