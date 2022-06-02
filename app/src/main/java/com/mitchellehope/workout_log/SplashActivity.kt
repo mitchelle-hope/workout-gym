90e1d39bbe37c1507a9bbe38c4ec49f28eaf01a7
mitchelle-hope <mitchellehopewaithera@gmail.com>
2022-06-02 20:39:14 +0300

    added login page

--- app/src/main/java/com/mitchellehope/workout_log/SplashActivity.kt
+++ app/src/main/java/com/mitchellehope/workout_log/SplashActivity.kt
@@ -1,10 +1,12 @@
 package com.mitchellehope.workout_log
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startActivity(Intent(this,LoginActivity::class.java))
     }
 }
