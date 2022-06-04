46ecbc1ab785bc62b7f4451d6b45aac34a3a5454
mitchelle-hope <mitchellehopewaithera@gmail.com>
2022-06-04 08:59:45 +0300

    sign up page

--- app/src/main/java/com/mitchellehope/workout_log/LoginActivity.kt
+++ app/src/main/java/com/mitchellehope/workout_log/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.mitchellehope.workout_log
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -13,6 +15,7 @@ class LoginActivity : AppCompatActivity() {
     lateinit var etEmail: TextInputEditText
     lateinit var tilPassword:TextInputLayout
     lateinit var etPassword:TextInputEditText
+    lateinit var tvSignup:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -22,8 +25,13 @@ class LoginActivity : AppCompatActivity() {
         etEmail = findViewById(R.id.etEmail)
         tilPassword = findViewById(R.id.tilPassword)
         etPassword = findViewById(R.id.etPassword)
+        tvSignup=findViewById(R.id.tvSignup)
 
         btnLogin.setOnClickListener { validateLogin() }
+        tvSignup.setOnClickListener{
+            var intent=Intent(this,Sign_upActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
--- app/src/main/java/com/mitchellehope/workout_log/Sign_upActivity.kt
+++ app/src/main/java/com/mitchellehope/workout_log/Sign_upActivity.kt
@@ -2,10 +2,91 @@ package com.mitchellehope.workout_log
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class Sign_upActivity : AppCompatActivity() {
+    lateinit var tilFirstname:TextInputLayout
+    lateinit var etFirstName:TextInputEditText
+    lateinit var tilLastname:TextInputLayout
+    lateinit var etLastname:TextInputEditText
+    lateinit var tilSignemail:TextInputLayout
+    lateinit var etSignemail:TextInputEditText
+    lateinit var tilPass:TextInputLayout
+    lateinit var etPass:TextInputEditText
+    lateinit var tilConfirm:TextInputLayout
+    lateinit var etConfirm:TextInputEditText
+    lateinit var tvSignin:TextView
+    lateinit var tvLogin:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        tilFirstname = findViewById(R.id.tilFirstname)
+        etFirstName = findViewById(R.id.etFirstName)
+        tilLastname = findViewById(R.id.tilLastname)
+        etLastname = findViewById(R.id.etLastname)
+        tilSignemail = findViewById(R.id.tilSignemail)
+        etSignemail = findViewById(R.id.etSignemail)
+        tilPass = findViewById(R.id.tilPass)
+        etPass= findViewById(R.id.etPass)
+        tilConfirm = findViewById(R.id.tilConfirm)
+        etConfirm = findViewById(R.id.etConfirm)
+        tvSignin=findViewById(R.id.tvLogin)
+        tvLogin=findViewById(R.id.tvLogin)
+
+        tvSignin.setOnClickListener { validateLogin() }
+
+
     }
+
+    fun validateLogin(){
+        var error= false
+        tilSignemail.error=null
+        tilPass.error=null
+        var email=etSignemail.text.toString()
+        if (email.isBlank()) {
+            tilSignemail.error="Email is required"
+            error=true
+        }
+        var password =etPass.text.toString()
+        if (password.isBlank()) {
+            tilPass.error="password is required"
+            error =true
+
+        }
+        if(!error){
+
+        }
+        var firstname =etFirstName.text.toString()
+        if (password.isBlank()) {
+            tilFirstname.error="Your first name is required"
+            error =true
+
+
+            }
+        var lastname =etLastname.text.toString()
+        if (password.isBlank()) {
+            tilLastname.error="Your last name is required"
+            error =true
+
+
+
+        }
+        var confirmpass =etConfirm.text.toString()
+        if (password.isBlank()) {
+            tilLastname.error="Your last name is required"
+            error =true
+
+
+
+        }
 }
+
+
+    }
+
+
+
+
